c67fd9098c16aaf67748e41c543d4744a912a352
Jetsada316 <95394938+Jetsada316@users.noreply.github.com>
2022-02-07 23:10:21 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="http://3.bp.blogspot.com/-tUfCqpLuMMI/Vl-mHSIAzqI/AAAAAAAAAJk/YWKjEsSgP5g/s1600/d7401ad0a9f0b32582bbbeb283d236df-d4zln7u.png"
+          transition="scale-transition"
+          width="60"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn to="/"  text>
+        <span class="mr-2">Home</span>
+      </v-btn>
+       <v-btn to="/about"  text>
+        <span class="mr-2">about</span>
+      </v-btn>
+       <v-btn to="/Todo"  text>
+        <span class="mr-2">Todo</span>
+      </v-btn>
+    </v-app-bar>
+    
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/views/Todo.vue
@@ -0,0 +1,32 @@
+<template>
+
+  <div>
+    
+    <Me @Input="validate"></Me>
+    <Ja :J1="J1"></Ja>
+    </div>
+    
+</template>
+
+<script>
+import Me from "../components/Me.vue"
+import Ja from "../components/Ja.vue"
+export default {
+  data: () => ({
+    J1: [],
+  }),
+components :{Me,Ja},
+J1: "Input",
+  methods: {
+    validate(select, name) {
+      this.J1.push({
+        name: select + " : " + name,
+      });
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
